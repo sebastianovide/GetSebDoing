0aa36b4020feb35515559d4f9669f989fb5e3b5a
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2019-07-03 22:13:19 +0100

    renamed it to GSebD

--- bin/serve.sh
+++ bin/serve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GSD5_DIR=`pwd`
-TIDDLYWIKI5_DIR=$GSD5_DIR/../TiddlyWiki5
+TIDDLYWIKI5_DIR=$GSD5_DIR/node_modules/tiddlywiki
 
 export TIDDLYWIKI_PLUGIN_PATH=$GSD5_DIR/plugins
 export TW_SERVE_EDITION_PATH=$GSD5_DIR/editions/gsd5-server
