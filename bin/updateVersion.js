d9e54c30b443dd6dbd081382b8c5858bd380f188
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2019-07-03 20:44:50 +0100

    create output if not exists

--- bin/updateVersion.js
+++ bin/updateVersion.js
@@ -10,7 +10,13 @@ versionArray[2] = buildNumber;
 // write version in plugin.info
 const fs = require('fs');
 const pluginInfoPath = "./plugins/gsd5/core/plugin.info";
-const pluginInfo = JSON.parse(fs.readFileSync(pluginInfoPath));  
-pluginInfo.version = versionArray.join(".")
-fs.writeFileSync(pluginInfoPath, JSON.stringify(pluginInfo, "", 2))
-fs.writeFileSync("./output/APP_VERSION", pluginInfo.version)
+const pluginInfo = JSON.parse(fs.readFileSync(pluginInfoPath));
+pluginInfo.version = versionArray.join(".");
+fs.writeFileSync(pluginInfoPath, JSON.stringify(pluginInfo, "", 2));
+
+// save the version for git tagging
+const dir = './output';
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+fs.writeFileSync(dir + "/APP_VERSION", pluginInfo.version);
